3a0c102d21deeaf94119d3735be542f11428ebb4
Rui Barreto <ruibarreto07@outlook.com>
2025-01-17 12:05:48 +0000

    Registration and Login completed.

--- Frontend/bookswap/src/App.tsx
+++ Frontend/bookswap/src/App.tsx
@@ -1,23 +1,28 @@
-import React from "react";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+import NewRegistration from "./components/Authentication/RegistrationComponent";
+import Authentication from "./components/Authentication/LoginComponent";
+import NotFound from "./components/404";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <ToastContainer />
+
+        <div className="flex-grow container mx-auto">
+          <Routes>
+            <Route
+              path="/Authentication/Registration"
+              element={<NewRegistration />}
+            />
+            <Route path="/Authentication/Login" element={<Authentication />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ Frontend/bookswap/src/types/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  title: string;
+  author: string;
+  description: string;
+  genre: string;
+  status: string;
+  owner_id: string;
+}
